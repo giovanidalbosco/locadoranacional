0351a357ea133bfedca0ac7a984ce93856c4909e
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-11-10 14:20:05 -0300

    first commit

---
+++ src/main/java/br/univille/locadoranacional/LocadoranacionalApplication.java
@@ -0,0 +1,13 @@
+package br.univille.locadoranacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoranacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoranacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/locadoranacional/LocadoranacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.locadoranacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoranacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
