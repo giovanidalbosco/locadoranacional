c9f0bcdef74f64c97b18e476eebd89a82c76d848
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-11-21 14:53:38 -0300

    atualização

--- src/main/java/br/univille/locadoranacional/controller/ClienteController.java
+++ src/main/java/br/univille/locadoranacional/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,4 +33,20 @@ public class ClienteController {
         return new ModelAndView("cliente/form", dados);
     
    }
+   @GetMapping("/alterar/{id}")  
+   public ModelAndView alterar(@PathVariable("id") long id){
+     var umCliente = service.findById(id);
+     HashMap<String,Object> dados = new HashMap<>();
+     dados.put("cliente",umCliente);
+     return new ModelAndView("cliente/form",dados);
+}
+
+public ModelAndView delete(@PathVariable("id") long id){
+
+    service.delete(id);
+
+     return new ModelAndView("redirect:/clientes");
+
+ }
+
 }
