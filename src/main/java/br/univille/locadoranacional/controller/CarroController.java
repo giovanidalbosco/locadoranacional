d451c4d98b06a46ee702f96545c93cd9492e45cb
GIOVANI VALLE DALBOSCO <giovani.dalbosco@univille.br>
2022-11-30 16:04:28 -0300

    adição de imagem dos carros

--- src/main/java/br/univille/locadoranacional/controller/CarroController.java
+++ src/main/java/br/univille/locadoranacional/controller/CarroController.java
@@ -1,11 +1,18 @@
 package br.univille.locadoranacional.controller;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.univille.locadoranacional.entity.Carro;
@@ -34,10 +41,28 @@ public class CarroController {
     }
 
     @PostMapping(params = "form")
-    public ModelAndView salvar(Carro carro) {
+    public ModelAndView salvar(Carro carro, @RequestParam("file") MultipartFile file) {
+        if(file.getSize() != 0){
+            String caminho = servico.salvarFoto(file);
+            carro.setFoto(caminho);
+        }
+        
         servico.save(carro);
-
         return new ModelAndView("redirect:/carros");
+    }
+    
+    @GetMapping(value = "/image/{id}")
+    public @ResponseBody byte[] getImage(@PathVariable("id") long id){
+        try{
+            Carro carro = servico.getOne(id);
+            File file = new File(carro.getFoto());
+            byte[] bytes = new byte[(int) file.length()];
+            DataInputStream dis = new DataInputStream(new FileInputStream(file));
+            dis.readFully(bytes);
+            return bytes;
+        }catch (Exception e){
+            return new byte[0];
+        }
     }
 
     @GetMapping("/editar/{id}")
