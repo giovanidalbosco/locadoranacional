d9d60a8eccb7ba51f91e177e33aea6d10145a257
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-12-08 17:15:47 -0300

    Atualização

---
+++ src/main/resources/static/scriptAluguel.js
@@ -0,0 +1,20 @@
+(function() {
+    $("#tabaluguel").on("click", ".js-delete", function() {
+        let botaoClicado = $(this);
+        $("#btnsim").attr("data-id", botaoClicado.attr("data-id"));
+        $("#modalaluguel").modal("show");
+    });
+
+    $("#btnsim").on("click", function() {
+        let botaoSim = $(this);
+        let id = botaoSim.attr("data-id");
+        console.log(id)
+        $.ajax({
+            url: "/aluguel/deletar/" + id,
+            method: "GET",
+            success: function() {
+                window.location.href="/aluguel";
+            }
+        });
+    });
+})();
